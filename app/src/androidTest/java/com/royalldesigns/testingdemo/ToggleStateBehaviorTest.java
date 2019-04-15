90764d00a520ecee65a4ac287b3256706451624b
William Drumheller <william@royalldesigns.com>
2019-04-15 19:09:45 -0400

    Move small tests to android local test package, so they run only on this machine

--- app/src/androidTest/java/com/royalldesigns/testingdemo/ToggleStateBehaviorTest.java
+++ app/src/androidTest/java/com/royalldesigns/testingdemo/ToggleStateBehaviorTest.java
@@ -9,19 +9,16 @@ import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.After;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.io.Console;
 import java.io.File;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.*;
-import static org.hamcrest.core.IsNot.not;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
